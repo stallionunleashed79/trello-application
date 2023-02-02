f706a28a8fd5539f80e32a84421a66cf7fecbc54
Bharath Seshadri <chasethesky79@Bharaths-MacBook-Pro.local>
2023-02-02 09:02:02 -0600

    Added code changes for add new item button

---
+++ src/components/AddNewItem.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react';
+import { AddItemButton } from '../styles';
+
+type AddNewItemProps = {
+    onAdd: (text: string) => void;
+    toggleButtonText: string;
+    dark?: boolean;
+}
+
+export const AddNewItem = ({ onAdd, toggleButtonText, dark }: AddNewItemProps ) => {
+    const [ showForm, setShowForm ] = useState(false);
+    if (showForm) {
+
+    }
+    return <AddItemButton dark={dark} onClick={() => setShowForm(true)}>{toggleButtonText}</AddItemButton>
+}
