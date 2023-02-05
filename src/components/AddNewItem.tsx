8e7c2e3e47534651aefe578bbf71fe5e354af74f
Bharath Seshadri <chasethesky79@Bharaths-MacBook-Pro.local>
2023-02-05 16:12:58 -0600

    Implemented the add new item form component

--- src/components/AddNewItem.tsx
+++ src/components/AddNewItem.tsx
@@ -1,16 +1,21 @@
 import { useState } from 'react';
 import { AddItemButton } from '../styles';
+import { NewItemForm } from './NewItemForm';
 
 type AddNewItemProps = {
-    onAdd?: (text: string) => void;
+    onAdd: (text: string) => void;
     toggleButtonText: string;
     dark?: boolean;
 }
 
 export const AddNewItem = ({ onAdd, toggleButtonText, dark }: AddNewItemProps ) => {
     const [ showForm, setShowForm ] = useState(false);
+    const handleOnAdd = (text: string) => {
+        onAdd(text);
+        setShowForm(false);
+    }
     if (showForm) {
-
+      return <NewItemForm onAdd={(text) => handleOnAdd(text)}/>
     }
     return <AddItemButton dark={dark} onClick={() => setShowForm(true)}>{toggleButtonText}</AddItemButton>
 }
