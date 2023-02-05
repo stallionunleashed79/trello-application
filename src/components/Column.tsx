dbd18a7fdce2ebf27e53b35a3b0655cd7f97b7ab
Bharath Seshadri <chasethesky79@Bharaths-MacBook-Pro.local>
2023-02-05 16:39:34 -0600

    Implemented functionality to add new card dynamically in a column

--- src/components/Column.tsx
+++ src/components/Column.tsx
@@ -1,18 +1,18 @@
 import { ColumnContainer, ColumnTitle } from "../styles";
 import { AddNewItem } from "./AddNewItem";
 import { Card } from "./Card";
+import { useState } from 'react';
 
 type ColumnProps = {
     text: string;
 }
 
-const Column = ({ text }: ColumnProps) => 
-    <ColumnContainer>
+const Column = ({ text }: ColumnProps) => {
+const [ items, setItems ] = useState<Array<string>>(['Generate app scaffold', 'Learn Typescript', 'Begin to use static typing']);
+   return  <ColumnContainer>
         <ColumnTitle>{text}</ColumnTitle>
-        <Card text="Generate app scaffold"/>
-        <Card text="Learn Typescript"/>
-        <Card text="Begin to use static typing"/>
-        <AddNewItem dark={true} toggleButtonText="+ Add another card" onAdd={() => console.log(`NEW ITEM ADDED`)}/>
+        { items.map(item => <Card text={item}/>)}
+        <AddNewItem dark={true} toggleButtonText="+ Add another card" onAdd={(text) => setItems([ ...items, text ])}/>
     </ColumnContainer>
-
+}
 export default Column;
