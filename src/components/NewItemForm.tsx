404c31704f5dc189c195e6dc8ca651ce4dac6a10
Bharath Seshadri <chasethesky79@Bharaths-MacBook-Pro.local>
2023-02-10 12:13:04 -0600

    Defined the context provider for the application

--- src/components/NewItemForm.tsx
+++ src/components/NewItemForm.tsx
@@ -9,10 +9,15 @@ type NewItemFormProps = {
 export const NewItemForm = ({ onAdd }: NewItemFormProps) => {
     const [text, setText] = useState('');
     const inputRef = useFocus();
+    const handleEnter = (event: React.KeyboardEvent<HTMLInputElement>) => {
+        if (event.key === "Enter") { 
+            onAdd(text)
+         }
+    }
 
     return (
         <NewItemFormContainer>
-            <NewItemInput ref={inputRef} value={text} onChange={e => setText(e.target.value)}/>
+            <NewItemInput ref={inputRef} value={text} onChange={e => setText(e.target.value)} onKeyDown={handleEnter}/>
             <NewItemButton onClick={() => onAdd(text)}>Create</NewItemButton>
         </NewItemFormContainer>
     )
---
+++ src/state/AppStateContext.tsx
@@ -0,0 +1,71 @@
+import { createContext, FC, useContext } from 'react'
+
+type Task = {
+    id: string;
+    text: string;
+}
+
+type List = {
+    id: string;
+    text: string;
+    tasks: Array<Task>
+}
+
+export type AppState = {
+    lists: Array<List>
+}
+
+interface Props {
+    children: React.ReactNode;
+  }
+
+const appData: AppState = {
+    lists:[
+        {
+            id: '0',
+            text: 'To Do',
+            tasks: [
+                {
+                    id: 'c0',
+                    text: 'Generate App Scaffold'
+                }
+            ]
+        },
+        {
+            id: '1',
+            text: 'In Progress',
+            tasks: [
+                {
+                    id: 'c1',
+                    text: 'Learn Typescript'
+                }
+            ]
+        },
+        {
+            id: '2',
+            text: 'Done',
+            tasks: [
+                {
+                    id: 'c2',
+                    text: 'Begin to use static typing'
+                }
+            ]
+        }
+    ]
+}
+
+type AppStateContextProps = {
+    lists: Array<List>,
+    getTasksByListId: (id: string) => Array<Task>
+}
+
+const AppStateContext = createContext<AppStateContextProps>({} as AppStateContextProps)
+
+export const AppStateProvider: React.FC<Props> = ({ children }) => {
+    const { lists } = appData;
+    const getTasksByListId = (id: string) => lists.find(list => list.id === id)?.tasks ?? []
+
+    return (
+        <AppStateContext.Provider value={ { lists, getTasksByListId }}>{children}</AppStateContext.Provider>
+    )
+}
