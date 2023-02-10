4d6deb614c589143559227268b1a794d41734608
Bharath Seshadri <chasethesky79@Bharaths-MacBook-Pro.local>
2023-02-10 12:42:43 -0600

    Fixed the warning for react lists

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import { useAppState } from './state/AppStateContext';
 export function App() {
   const { lists } = useAppState()
   return <AppContainer>
-    {lists.map(({ id, text }) => <Column text={text} id={id}/>)}
+    {lists.map(({ id, text }) => <Column key={id} text={text} id={id}/>)}
     <AddNewItem toggleButtonText="+ Add another list" onAdd={() => console.log('LIST ITEM ADDED')}/>
   </AppContainer>;
 }
