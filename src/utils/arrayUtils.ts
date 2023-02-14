561fdbef0b240dccbc337d9cfeb5f8529e4d4d6a
Bharath Seshadri <chasethesky79@bharaths-mbp.lan>
2023-02-14 11:18:04 -0600

    Added 2 utility methods to add and remove items from index

--- src/utils/arrayUtils.ts
+++ src/utils/arrayUtils.ts
@@ -4,3 +4,7 @@ type Item = {
 }
 
 export const findItemIndexById = <T extends Item>(items: T[], id: string) => items.findIndex(item => item.id === id)
+
+export const removeItemFromIndex = <T extends Item>(items: T[], index: number) => [...items.slice(0, index), items.slice(index+1)];
+
+export const addItemAtIndex = <T extends Item>(items: T[], item: T, index: number) => [...items.slice(0, index), item, ...items.slice(index)]
